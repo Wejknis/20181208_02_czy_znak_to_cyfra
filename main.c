d137c018ab81806f6184122a225aab999497254c
Wejknis <weja077@gmail.com>
2018-12-08 10:04:34 +0100

    1 2 3 start end

--- main.c
+++ main.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
+#define isDigit(c) ((c) >= '0' && (c) <= '9')
 int main() {
-    printf("Hello, World!\n");
+    char znak = '1';
+
+    if(isDigit(znak)){
+        printf("jestem cyfrą");
+    } else{
+        printf("nie jestem cyfrą");
+    }
     return 0;
 }
